134ccdb3560b6c746f4aa4afaca2bcd79ec71d24
hiroshi <hiroshi3110@gmail.com>
2018-01-17 21:02:01 +0900

    Prepend 'q=' for query_tag of histories.

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -36,7 +36,7 @@ class MessagesChannel < ApplicationCable::Channel
     )
     # save query history
     if q.present? && data['save']
-      query_tag = q.strip.gsub(/\s+/,'+')
+      query_tag = 'q=' + q.strip.gsub(/\s+/,'+')
       query_message = room.messages.tags('query', query_tag).first || room.messages.build
       query_message.update!(content: <<~CONTENT, user: current_user)
         #{q}
